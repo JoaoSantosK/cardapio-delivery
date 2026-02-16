e44815a85938488b5b049c5c4302ef4976243b6f
João Santos <navijcsantos@gmail.com>
2026-02-16 17:26:45 -0300

    advances and modifications
    Avanços no projeto, implementação de novos botões e ajustes em classes HTML e IDs. Novas adições ao .js para a aparição da notificação, servindo de confirmação para o botão > "resetar pedido". Ajustes técnicos nos HTML, CSS e JS para otimizar e evitar quebras de código.

--- js/function.js
+++ js/function.js
@@ -1,17 +1,36 @@
-
+// Resetar o pedido
 var bntR = document.getElementById('btn-r');
-
 bntR.addEventListener('click', function() {
     var inputOrder = document.getElementById('input-order');
     inputOrder.value = '';
+
+    // Exibir notificação de pedido resetado
+    var notify = document.getElementById('notfR');
+    var parentNotify = document.getElementById('notf'); // Seleciona a div branca (pai)
+
+    
+    if (notify && parentNotify) {
+        notify.innerText = 'Pedido resetado com sucesso!';
+        parentNotify.style.display = 'block'; // Mostra a caixa da notificação
+
+        setTimeout(function() {
+            parentNotify.style.display = 'none'; // Esconde a notificação após 3 segundos
+        }, 3000);
+    }
 })
 
-var arrow = document.querySelector('.fa-arrow-left');
+// Resetar pedido | Exibir notificação
 
+
+
+// Finalizar pedido | Redirecionar para página de finalização
+var ordN = document.getElementById('ord-n');
+ordN.addEventListener('click', function() {
+    window.location.href = 'finalizado.html';
+});
+
+// Redirecionar para página anterior
+var arrow = document.querySelector('.fa-arrow-left');
 arrow.addEventListener('click', function () {
     window.location.href = 'algo.html';
 })
-
-var notify = document.getElementById('notfR');
-
-notify.innerText = 'Pedido resetado com sucesso!';
