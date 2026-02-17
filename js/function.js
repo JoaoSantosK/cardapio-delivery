cd9d19a4fb4a174b5c161260e06ea35846d4b4f7
João Santos <navijcsantos@gmail.com>
2026-02-17 11:29:24 -0300

    refinamento de funções
    Refinamento da função de notificação criando um fadeIn e um fadeOut para a notificação no JS e alteração no modo como ambos acontencem, antes por display, agora por opacity e visibility no CSS.

--- js/function.js
+++ js/function.js
@@ -1,27 +1,25 @@
 // Resetar o pedido
 var bntR = document.getElementById('btn-r');
+var notifyTimeout;
 bntR.addEventListener('click', function() {
     var inputOrder = document.getElementById('input-order');
     inputOrder.value = '';
 
-    // Exibir notificação de pedido resetado
     var notify = document.getElementById('notfR');
-    var parentNotify = document.getElementById('notf'); // Seleciona a div branca (pai)
+    var parentNotify = document.getElementById('notf');
 
-    
     if (notify && parentNotify) {
         notify.innerText = 'Pedido resetado com sucesso!';
-        parentNotify.style.display = 'block'; // Mostra a caixa da notificação
 
-        setTimeout(function() {
-            parentNotify.style.display = 'none'; // Esconde a notificação após 3 segundos
+        parentNotify.classList.add('show'); // Exibe a notificação
+
+        clearTimeout(notifyTimeout); // Limpa o timeout anterior, se houver
+
+        notifyTimeout = setTimeout(() => { // Esconde a notificação após 3 segundos
+            parentNotify.classList.remove('show');
         }, 3000);
     }
-})
-
-// Resetar pedido | Exibir notificação
-
-
+});
 
 // Finalizar pedido | Redirecionar para página de finalização
 var ordN = document.getElementById('ord-n');
