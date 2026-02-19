082ae10712877f6cbfb83670b0848b32d312da22
João Santos <navijcsantos@gmail.com>
2026-02-19 16:52:08 -0300

    Adição de order.js
    Adicionado o arquivo order.js com a função de ter os pedidos e exportá-lo ao function.js de forma dinâmica para pegar as informações corretas sobre o pedido do cliente referente aos botões do index.html

---
+++ js/order.js
@@ -0,0 +1,92 @@
+export class Pedido {
+    constructor() {
+        this.size = null;
+        this.flavor = null;
+        this.tborder = null;
+        this.border = null;
+        this.drink = null;
+        this.prices = {
+            Pequeno: 25,
+            Médio: 35,
+            Grande: 45,
+            "Família": 55,
+            "Super Família": 65
+        };
+    }
+
+    setSize(size) {
+        if (!this.prices[size]) {
+            console.warn("Tamanho inválido.");
+            return;
+        }
+        this.size = size;
+    }
+
+    setFlavor(flavor) {
+        this.flavor = flavor;
+    }
+
+    setTypeBorder(tborder) {
+        this.tborder = tborder;
+    }
+
+    setBorder(border) {
+        this.border = border;
+    }
+
+    setDrink(drink) {
+        this.drink = drink;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    getFlavor() {
+        return this.flavor;
+    }
+
+    getTypeBorder() {
+        return this.tborder;
+    }
+
+    getBorder() {
+        return this.border;
+    }
+
+    getDrink() {
+        return this.drink;
+    }
+
+    getPrice() {
+        if (!this.size) return 0;
+        return this.prices[this.size];
+    }
+
+    isValid() {
+        return this.size !== null && this.flavor !== null;
+    }
+
+    reset() {
+        this.size = null;
+        this.flavor = null;
+        this.drink = null;
+    }
+
+    finalizar() {
+        if (!this.isValid()) {
+            console.warn("Pedido incompleto.");
+            return false;
+        }
+
+        const resumo = {
+            size: this.size,
+            flavor: this.flavor,
+            drink: this.drink,
+            total: this.getPrice()
+        };
+
+        localStorage.setItem("pedido", JSON.stringify(resumo));
+        return true;
+    }
+}
