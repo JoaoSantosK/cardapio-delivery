12b4f2f1195c91a37b9e1210831a09535098bdc9
João Santos <navijcsantos@gmail.com>
2026-02-19 16:53:25 -0300

    Adição do stock.js
    Adicionado o arquivo stock.js para definir todo o estoque de produtos do clientes. Aplicado em um projeto real, haveria a substituição por uma API própria para tais comportamentos.

---
+++ js/stock.js
@@ -0,0 +1,14 @@
+const stock = {
+    flavors: {
+        Calabresa: true,
+        "Frango com Catupiry": true,
+        Portuguesa: true,
+        "Quatro queijos": true,
+        Vegetariana: true,
+    },
+    drinks: {
+        "Coca-Cola": true,
+        Pepsi: true,
+        "Guaraná Antarctica": false, // Indisponível
+    }
+}
